fa72cdd62eff9570e62276ec56b8755296ff2f31
Lesliekonlack <122942229+Lesliekonlack@users.noreply.github.com>
2024-04-12 19:38:04 +0000

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,21 @@
+<?php
+// Database credentials
+    $servername = "localhost";
+    $username = "root";
+    $password = "QlAlrs+N8Gt9";
+    $dbname = "EzTickEntry";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    // Check connection
+    if ($conn->connect_error) {
+        echo "Error connecting";
+    } else {
+        echo "Connected";
+    }
+
+// Always good to close the database connection if not needed anymore
+
+?>
 
