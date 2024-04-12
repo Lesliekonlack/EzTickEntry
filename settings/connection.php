44a6e916ace173e7bbfe6bd995697c960226f790
Lesliekonlack <122942229+Lesliekonlack@users.noreply.github.com>
2024-04-12 11:06:17 +0000

    Update connection.php

--- settings/connection.php
+++ settings/connection.php
@@ -1,18 +1,16 @@
-<?php
-// Defining database connection parameters as constants
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'QlA]rs+N8Gt9');
-define('DB_NAME', 'EzTickEntry');
-
-// Attempting to connect to MySQL database
-$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS , DB_NAME);
-
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    $successMessage = "Connected successfully"; // Storing the success message in a variable
-    // return $successMessage; // Returning the success message
-    // echo "success" ;
-}
+ <?php
+// Database credentials
+   $servername = "localhost";
+   $username = "root";
+   $password = "QlA]rs+N8Gt9";
+   $dbname = "EzTickEntry";
+   
+   // Create connection
+   $conn = new mysqli($servername, $username, $password, $dbname);
+   
+   // Check connection
+   if ($conn->connect_error) {
+       echo "Error connecting";
+   }
+   
 ?>
